4849ff063dc1ad3a1c4348554f7b3fe5c6e246e5
EBINAtettyan <tetsuya.ebina1016@gmail.com>
2018-10-10 17:14:45 +0900

    更新機能（エラーで返せるけど、updateでなく、insertされてしまう）

--- src/main/java/com/example/baseball/service/PlayerService.java
+++ src/main/java/com/example/baseball/service/PlayerService.java
@@ -15,26 +15,30 @@ public class PlayerService {
 	 * @Autowiredを付けて宣言するとnewしなくてもインスタンス化して使うことができる
 	 */
 
-    @Autowired
-    private PlayerRepository playerRepository;
+	@Autowired
+	private PlayerRepository playerRepository;
 
-    public List<Player> findAll() {
-    	List<Player> playerList =  playerRepository.findAll();
-        return playerList;
-    }
+	public List<Player> findAll() {
+		List<Player> playerList = playerRepository.findAll();
+		return playerList;
+	}
 
-    public Player findOne(Integer id) {
-        // メソッド名がfindOneからfindByIdに変わった
-        // Optionalを返すようになったので取得できなかった場合の処理`.orElse(null)`を追加
-        return playerRepository.findById(id).orElse(null);
-    }
+	public Player findOne(Integer id) {
+		// メソッド名がfindOneからfindByIdに変わった
+		// Optionalを返すようになったので取得できなかった場合の処理`.orElse(null)`を追加
+		return playerRepository.findById(id).orElse(null);
+	}
 
-    public Player save(Player player) {
-        return playerRepository.save(player);
-    }
+	public Player save(Player player) {
+		return playerRepository.save(player);
+	}
 
-    public void delete(Integer id) {
-        // メソッド名がdeleteからdeleteByIdに変わった
-        playerRepository.deleteById(id);
-    }
+	public void delete(Integer id) {
+		// メソッド名がdeleteからdeleteByIdに変わった
+		playerRepository.deleteById(id);
+	}
+
+	public Player update(Player player) {
+		return playerRepository.save(player);
+	}
 }
